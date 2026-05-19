7f35fff3dc68669eb8da25c142064e065516f672
mitm22452 <mitm22452@gmail.com>
2026-05-19 16:05:39 +0530

    Chapter 0: Built the boilerplate

---
+++ 0-Intro/first_code.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("Hello World!");
+    return 0;
+}
+
+// Here, #include <stdio.h> is a preprocessor directive in C that tells the compiler to include the Standard Input Output header file before compiling your code. It tells the compiler to take a few functions from that directive and include in our program.
+
+// int main() is a funciton. Every program in C starts from main funtions. By assigning int we define that the output of this function will be an integer.
+
+// {} is the main canvas where we write what this function will do. Like adding numbers, multiplying integers, etc. In this function, we are printing "Hello World!" using the "printf" function.
+
+// When we want to end a specific task in C, we use ";" at the end.
+
+// return 0 tells the compiler to end the program.
